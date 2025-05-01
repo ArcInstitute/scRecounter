c3c45bbd6d029b513c41f9f827f9cf536bdbe77c
nick-youngblut <nyoungb2@gmail.com>
2025-04-30 21:21:39 -0700

    set db name for cleanup script in entrypoint script

--- docker/sc-recounter-run/entrypoint.sh
+++ docker/sc-recounter-run/entrypoint.sh
@@ -10,11 +10,12 @@ PROFILE_LIST=$(IFS=,; echo "$*")
 if [ "$DEPLOYMENT" == "test" ]; then
   WORK_DIR="gs://arc-ctc-nextflow/scRecounter/test/work/${RUN_NAME}"
   OUTPUT_DIR="gs://arc-ctc-screcounter/test/${RUN_NAME}"
+  export GCP_SQL_DB_NAME="sragent-test"
 else  
   WORK_DIR="gs://arc-ctc-nextflow/scRecounter/prod/work/${RUN_NAME}"
   OUTPUT_DIR="gs://arc-ctc-screcounter/prod4/${RUN_NAME}"
+  export GCP_SQL_DB_NAME="sragent-prod"
 fi
-
 
 micromamba run -n sc-recounter-run \
   nextflow run main.nf \
@@ -26,7 +27,6 @@ micromamba run -n sc-recounter-run \
 
 # Delete output directory if only nf-report and nf-trace
 export GCP_SQL_DB_HOST="35.243.133.29"
-export GCP_SQL_DB_NAME="sragent-prod"
 export GCP_SQL_DB_USERNAME="postgres"
 micromamba run -n sc-recounter-run \
   python cleanup.py $WORK_DIR $OUTPUT_DIR
